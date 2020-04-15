d75e1f33afdff29addd53a9ff0a7164f5e47f08e
Hugobox <hugobox@gmail.com>
2020-04-15 10:29:45 -0400

    Better cleaning of non-clonable elements

--- google.js
+++ google.js
@@ -137,9 +137,7 @@ module.exports = function(RED) {
                     shape: 'dot',
                     text: 'success'
                 });
-
-                msg.payload = res;
-                delete msg.payload.request
+                msg.payload = res.data;
                 node.send(msg);
             });
 
